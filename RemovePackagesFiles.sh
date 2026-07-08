9561917cc6e1a9f1e7f84446f3816ededa2fd8d3
braam <44501894+braam@users.noreply.github.com>
2026-07-08 14:34:34 +0200

    Create RemovePackagesFiles.sh

---
+++ RemovePackagesFiles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -rf /phion0/packages/*.tgz
+rm -rf /phion0/packages/kl/rpm/*
+rm -rf /phion0/packages/os/rpm/*
+rm -rf /phion0/packages/ph/rpm/*
+rm -rf /phion0/packages/sa/*
+rm -rf /phion0/packages/tgz/*
